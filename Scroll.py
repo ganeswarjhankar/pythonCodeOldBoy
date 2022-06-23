b6866a167e8d2c29a55df4b99e31f3f70677b528
ganeswar Jhankar <ganeswarjhankar@gmail.com>
2022-06-23 19:01:58 +0530

    check by GJ

--- Scroll.py
+++ Scroll.py
@@ -3,7 +3,7 @@ from selenium.webdriver.chrome.service import Service
 
 from selenium .webdriver.common.by import By
 #from selenium.webdriver.support.ui import Select
-from selenium.common.exceptions.exceptions
+
 
 import time
 
@@ -18,15 +18,15 @@ driver.maximize_window()
 
 #driver.execute_script("window.scrollBy(0,3100)","")
 
-Appointment = driver.find_element(By.LINK_TEXT,("Get a free second opinion from top surgeons! Book an appointment »")).click()
+#Appointment = driver.find_element(By.LINK_TEXT,("Get a free second opinion from top surgeons! Book an appointment »")).click()
 #driver.find_element(By.XPATH,"//*[@id='leadname2']").send_keys("Form1_PatientName")
 #driver.find_element(By.ID,"contactnum2").send_keys("7896544568")
 
 
-driver.switch_to.frame(driver.find_element(By.XPATH,"//*[@id='appointmodal']/div/div/div[2]"))
-driver.find_element(By.ID,"leadquery").send_keys("Form1_PatientName is  test")
+#driver.switch_to.frame(driver.find_element(By.XPATH,"//*[@id='appointmodal']/div/div/div[2]"))
+#driver.find_element(By.ID,"leadquery").send_keys("Form1_PatientName is  test")
 
-driver.find_element(By.XPATH,"//*[@id='closemodal']").click()
+#driver.find_element(By.XPATH,"//*[@id='closemodal']").click()
 
 time.sleep(5)
 
@@ -37,6 +37,29 @@ Consultation = driver.find_element(By.CLASS_NAME,"consultation-form")
 driver.execute_script("arguments[0].scrollIntoView();",Consultation)
 driver.find_element(By.CLASS_NAME,"form-control").send_keys("TestCheck")
 driver.find_element(By.CSS_SELECTOR,"input[id='contactnumhome']").send_keys("9438857617")
+driver.find_element(By.XPATH,"//*[@id='leadcityhome']/option[2]").click()
+driver.find_element(By.XPATH,"//*[@id='leadqueryhome']").send_keys("This is a Good Query test By GJ")
+
+time.sleep(5)
+#scroll
+BookNow = driver.find_element(By.ID,"LeadSubmitNewHome1")
+driver.execute_script("arguments[0].click();",BookNow)
+time.sleep(5)
+driver.back()
+
+#driver.find_element(By.ID,"LeadSubmitNewHome1").click()
+
+#Thank you page Validation element and attributes
+#driver.find_element(By.XPATH,"/html/body/div/div/div/div/a[1]").click()
+#driver.find_element(By.CLASS_NAME,"call-toll").click()
+
+#backward move to the page
+
+#Whatsapp button check (Optional Method)
+##driver.find_element(By.XPATH,"/html/body/div/div/div/div/a[2]/span").click()
+
+
+
 
 #DropDown = Select(driver.find_element(By.ID,""))
 
@@ -45,6 +68,8 @@ Consultation1 = driver.find_element(By.CLASS_NAME,"btn-round-primary")
 driver.execute_script("arguments[0].scrollIntoView();",Consultation1)
 
 driver.find_element(By.CSS_SELECTOR,"input[id='contactnum1']").send_keys("9438857617")
+driver.find_element(By.XPATH,"//*[@id='LeadSubmit1']")
+
 
 #Delhi=driver.find_element(By.CSS_SELECTOR,"input[text=' Delhi - NCR']")
 
---
+++ Seach and Browser.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+
+S = Service("D:\\chromedriver.exe")
+driver = webdriver.Chrome(service=S)
+
