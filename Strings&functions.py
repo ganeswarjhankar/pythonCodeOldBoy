015e868781b20d30495d9f2673f09900c84a0580
ganeswar Jhankar <ganeswarjhankar@gmail.com>
2022-06-29 18:49:43 +0530

    strings concepts by gj

---
+++ Strings&functions.py
@@ -0,0 +1,22 @@
+str = "RahulShettyAcademy.com & GaneswarJhankar"
+str1 = "String 1 firm"
+str3 = "GaneswarJhankar"
+print(str[1]) #string character check only a
+print(str[0:5]) #if you want to get sub strings
+
+print(str+str1)  #concatenation
+
+print(str3 in str)  #substring checkr
+
+var = str.split(".")
+print(var)
+print((var[0]))
+
+str4 = "    great     "
+print(str4.strip())
+print(str4.lstrip())
+print(str4.rstrip())
+
+
+
+
