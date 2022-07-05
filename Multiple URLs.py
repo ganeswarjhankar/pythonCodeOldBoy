2da552b7c7711dfcc04ebc4b87baedf0ba6068f0
ganeswar Jhankar <ganeswarjhankar@gmail.com>
2022-07-05 11:22:46 +0530

    strings concepts by gj

--- Multiple URLs.py
+++ Multiple URLs.py
@@ -4,4 +4,4 @@ driver = webdriver.Chrome(executable_path="D:\\chromedriver.exe")
 driver.get("https://www.hexahealth.com/ ")
 
 Inputboxes=driver.find_elements_by_class_name()
-Print (len(inputboxes)
+Print (len(inputboxes)....
