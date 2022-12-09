767f474c584da9ed354034bd74933b5e778b7091
ganeswar <ganeswarjhankar@gmail.com>
2022-12-09 17:46:34 +0530

    commit in the UsaBranch

--- Marketing.py
+++ Marketing.py
@@ -12,3 +12,118 @@ driver=webdriver.Chrome(service=S)
 URL=driver.get("https://www.hexahealth.com/marketing/lasik-bangalore")
 #driver.implicitly_wait(3)
 driver.maximize_window()
+
+  IS working in hexahealth.com and automation project 123
+
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from selenium .webdriver.common.by import By
+#from selenium.webdriver.support.ui import Select
+#from selenium.webdriver.common.action_chains import ActionChains
+import time
+S=Service("D:\\chromedriver.exe")
+driver=webdriver.Chrome(service=S)
+
+
+URL=driver.get("https://www.hexahealth.com/marketing/lasik-bangalore")
+#driver.implicitly_wait(3)
+driver.maximize_window()
+
+  IS working in hexahealth.com and automation project 123
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from selenium .webdriver.common.by import By
+#from selenium.webdriver.support.ui import Select
+#from selenium.webdriver.common.action_chains import ActionChains
+import time
+S=Service("D:\\chromedriver.exe")
+driver=webdriver.Chrome(service=S)
+
+
+URL=driver.get("https://www.hexahealth.com/marketing/lasik-bangalore")
+#driver.implicitly_wait(3)
+driver.maximize_window()
+
+  IS working in hexahealth.com and automation project 123
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from selenium .webdriver.common.by import By
+#from selenium.webdriver.support.ui import Select
+#from selenium.webdriver.common.action_chains import ActionChains
+import time
+S=Service("D:\\chromedriver.exe")
+driver=webdriver.Chrome(service=S)
+
+
+URL=driver.get("https://www.hexahealth.com/marketing/lasik-bangalore")
+#driver.implicitly_wait(3)
+driver.maximize_window()
+
+  IS working in hexahealth.com and automation project 123
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from selenium .webdriver.common.by import By
+#from selenium.webdriver.support.ui import Select
+#from selenium.webdriver.common.action_chains import ActionChains
+import time
+S=Service("D:\\chromedriver.exe")
+driver=webdriver.Chrome(service=S)
+
+
+URL=driver.get("https://www.hexahealth.com/marketing/lasik-bangalore")
+#driver.implicitly_wait(3)
+driver.maximize_window()
+
+  IS working in hexahealth.com and automation project 123
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from selenium .webdriver.common.by import By
+#from selenium.webdriver.support.ui import Select
+#from selenium.webdriver.common.action_chains import ActionChains
+import time
+S=Service("D:\\chromedriver.exe")
+driver=webdriver.Chrome(service=S)
+
+
+URL=driver.get("https://www.hexahealth.com/marketing/lasik-bangalore")
+#driver.implicitly_wait(3)
+driver.maximize_window()
+
+  IS working in hexahealth.com and automation project 123
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from selenium .webdriver.common.by import By
+#from selenium.webdriver.support.ui import Select
+#from selenium.webdriver.common.action_chains import ActionChains
+import time
+S=Service("D:\\chromedriver.exe")
+driver=webdriver.Chrome(service=S)
+
+
+URL=driver.get("https://www.hexahealth.com/marketing/lasik-bangalore")
+#driver.implicitly_wait(3)
+driver.maximize_window()
+
+  IS working in hexahealth.com and automation project 123
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+from selenium .webdriver.common.by import By
+#from selenium.webdriver.support.ui import Select
+#from selenium.webdriver.common.action_chains import ActionChains
+import time
+S=Service("D:\\chromedriver.exe")
+driver=webdriver.Chrome(service=S)
+
+
+URL=driver.get("https://www.hexahealth.com/marketing/lasik-bangalore")
+#driver.implicitly_wait(3)
+driver.maximize_window()
+
+  IS working in hexahealth.com and automation project 123
